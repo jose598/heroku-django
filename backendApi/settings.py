1277f7567ba19e73b63e64d9ac1f80cad071f919
jose598 <morales2010mendoza@hotmail.com>
2020-08-22 12:47:02 -0500

    ch(deploy): Setup deploy to heroku

--- backendApi/settings.py
+++ backendApi/settings.py
@@ -1,6 +1,7 @@
 
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
 ACCOUNT_EMAIL_VERIFICATION='none'
 ACCOUNT_EMAIL_REQUIRED=True
 ACCOUNT_AUTHENTICATION_METHOD='username'
